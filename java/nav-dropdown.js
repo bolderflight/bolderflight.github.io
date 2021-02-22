b84fe8858a45451216aacd76d9d56a5cc381d299
Melissa Iszler <melissa.iszler@bolderflight.com>
2021-02-22 10:39:51 -0800

    MQI: added dropdown menu for small screens, customer images on Home

--- java/nav-dropdown.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const nav = document.getElementsByClassName('nav')[0]
-
-toggleButton.addEventListener('click', () => {
-  nav.classList.toggle('active')
-})
